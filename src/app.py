116c9a9c5514eb918edd2b1ed2fc8174ca65f423
abnerthen <then.abner@gmail.com>
2024-11-06 11:44:54 +0800

    Add sunburst dashboard & fix threading issue

--- src/app.py
+++ src/app.py
@@ -98,5 +98,5 @@ def open_browser():
     webbrowser.open_new("http://127.0.0.1:8050")
 
 if __name__ == "__main__":
-    threading.Thread(1, open_browser).run()
+    threading.Timer(1, open_browser).run()
     app.run_server()
